5773c882d668ac34f1447df2f6e7bff29b32dbec
Aniruddha Katkar <aniruddhakatkar5@gmail.com>
2023-08-21 10:55:16 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,22 @@
+
+public class Swapping
+{
+ public static void main(String[]args)
+  {  
+
+    int num1 = 10, num2 = 20;
+    
+    System.out.println("----Before Swapping----");
+    System.out.println("First Number is :" +num1);
+    System.out.println("Second Number is :" +num2);
+
+
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+        System.out.println("----After Swapping----");
+    System.out.println("First Number is :" +num1);
+    System.out.println("Second Number is :" +num2);
+
+  }
+}
